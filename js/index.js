d8074ce1f2a88c3588011b4e79a1d2a5ff7f29d1
Toby <curti024@csusm.edu>
2016-04-23 14:32:37 -0700

    added CSS and the images. only struggle remaining is the button

--- js/index.js
+++ js/index.js
@@ -29,52 +29,13 @@ $(document).ready
           difference = Math.round(difference * 100) / 100;
         }
       }
+      
        $("#outputDollars").text(currencies[0]);
        $("#outputQuarters").text(currencies[1]);
        $("#outputDimes").text(currencies[2]);
        $("#outputNickels").text(currencies[3]);
        $("#outputPennies").text(currencies[4]);                  
       
- alert(oDifference + " is the total amount of change, and you get " + currencies[0] + " dollars " + currencies[1] + " quarters " + currencies[2] + " dimes " + currencies[3] + " nickels " + currencies[4] + " pennys");
-      
-      
-//      
-//      while( 1 <= difference )
-//      {
-//        dollar++;
-//        difference--;
-//        difference = Math.round(difference * 100) / 100;
-//      }
-//      
-//      while( 0.25 <= difference )
-//      {
-//        
-//        quarter++;
-//        difference -= 0.25;
-//        difference = Math.round(difference * 100) / 100;
-//      }
-//      
-//       while( 0.10 <= difference )
-//      {    
-//        dime++;
-//        difference -= 0.10;
-//        difference = Math.round(difference * 100) / 100;
-//      }
-//      
-//       while( 0.05 <= difference) 
-//      {        
-//        nickel++;
-//        difference -= 0.05;
-//        difference = Math.round(difference * 100) / 100;
-//      }
-//       
-//      while( 0.01 <= difference )
-//      { 
-//        penny++;
-//        difference -= 0.01; 
-//        difference = Math.round(difference * 100) / 100;
-//      }
- 
 
     }  
   
